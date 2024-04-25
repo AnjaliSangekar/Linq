2d71f433141d247c0f36fc179e4f665f26845e4c
dell <anjalisangekar1998@gmail.com>
2024-04-25 17:08:25 +0530

    Add project files.

---
+++ Linq/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>()
+            {
+                new Book() { Title = "ADO.Net book", Price = 2 },
+                new Book() { Title = "ASP.Net book", Price = 4 },
+                new Book() { Title = "C#.Net book", Price = 10 },
+                new Book() { Title = "ASP.Net book", Price = 6 },
+                new Book() { Title = "C#.Net book", Price = 18 }
+
+            };
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           var books = new BookRepository().GetBooks();
+
+            //
+            // LINQ Quary operator
+
+            //var cheaperbook = from b in books 
+            //                  where b.Price < 10 
+            //                  orderby b.Title
+            //                  select b.Title;
+
+
+
+            //
+            // LINQ extension method
+            //var cheapbook = books.Where(b => b.Price < 10).OrderBy(b => b.Title).Select(b => b.Title);
+
+
+
+            //
+            //var cheapbook = new List<Book>();
+
+            //foreach (var book in books)
+            //{
+            //    if(book.Price < 10)
+            //        cheapBook.Add(book);
+            //}
+
+
+            //foreach (var book in cheapbook)
+            //{
+            //    Console.WriteLine(book);
+            //    //Console.WriteLine(book.Title + " " + book.Price);
+            //}
+
+
+            //
+
+            var book = books.Single(b => b.Title == "ADO.Net book");
+
+            Console.WriteLine(book.Title);
+
+            // methods :
+            // skip, take,count, max, min, sum, count, average
+            //  first, last, single, where, singleordefault, lastordefualt
+
+
+            var maxprice = books.Max(b => b.Price);
+            Console.WriteLine(maxprice);
+            var minprice = books.Min(b => b.Price);
+            Console.WriteLine(minprice);
+
+
+
+        }
+    }
+}
